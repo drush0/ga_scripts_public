586b557552d9fd0e6cd8f2d0be5f5a8f938747b6
Daire Rush <d.rush@live.ie>
2020-12-04 23:47:04 -0500

    Create credentials.py

---
+++ ga_cnxn/credentials.py
@@ -0,0 +1,9 @@
+# This is a purposly blank file. I used the method described in the URL below to generate these variables:
+# https://www.themarketingtechnologist.co/getting-started-with-the-google-analytics-reporting-api-in-python/ 
+
+client_id = ''
+client_secret= ''
+redirect_uri = ''
+access_code = ''
+access_token = ''
+refresh_token = ''
